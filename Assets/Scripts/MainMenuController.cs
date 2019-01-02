27c2f0ac593490da827a5bb08e6ca92d1cef5d58
elektr90 <patryk.pajdosz@gmail.com>
2019-01-02 21:42:10 +0100

    added simple MainMenu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadGameplayScene()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
