e95bff4a0b955272c9e5a8f127a9a4375f7f1a1b
elektr90 <patryk.pajdosz@gmail.com>
2019-01-03 17:05:49 +0100

    added picture circle.psd

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float playerSpeed;
+    private Rigidbody2D playerRigidbody;
+
+    private void Start()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>(); 
+
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            playerRigidbody.velocity = playerSpeed * Vector2.up;
+
+
+
+        }
+
+
+    }
+
+
+}
